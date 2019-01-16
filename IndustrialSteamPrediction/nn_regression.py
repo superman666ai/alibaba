00b25f2cd650af83a0d811deddd8cf5b4ae93ed7
keepingoner <keepingoner@163.com>
2019-01-16 16:51:25 +0800

    d

--- IndustrialSteamPrediction/nn_regression.py
+++ IndustrialSteamPrediction/nn_regression.py
@@ -14,8 +14,8 @@ df = pd.read_csv('data/zhengqi_train.txt', sep='\t')
 test_df = pd.read_csv('data/zhengqi_test.txt', sep='\t')
 
 # 剔除认为不重要的特征
-df.drop(['V5', 'V17', 'V28', 'V22', 'V11', 'V9'], axis=1, inplace=True)
-test_df.drop(['V5', 'V17', 'V28', 'V22', 'V11', 'V9'], axis=1, inplace=True)
+# df.drop(['V5', 'V17', 'V28', 'V22', 'V11', 'V9'], axis=1, inplace=True)
+# test_df.drop(['V5', 'V17', 'V28', 'V22', 'V11', 'V9'], axis=1, inplace=True)
 
 # 数据处理
 
@@ -24,11 +24,11 @@ y = df["target"]
 
 y = np.array(y)[:, np.newaxis]
 
-x_mm = StandardScaler()
+x_mm = MinMaxScaler()
 x = x_mm.fit_transform(x)
 test_df = x_mm.fit_transform(test_df)
 
-y_mm = StandardScaler()
+y_mm = MinMaxScaler()
 y = y_mm.fit_transform(y)
 
 # 数据集划分
@@ -51,24 +51,25 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 
 
 xs = tf.placeholder(shape=[None, x_train.shape[1]], dtype=tf.float32, name="inputs")
-
 ys = tf.placeholder(shape=[None, 1], dtype=tf.float32, name="y_true")
+drop_out = 0.6
 
-# l1 = add_layer(xs, x_train.shape[1], 100, activation_function=tf.nn.sigmoid)
-l1 = add_layer(xs, x_train.shape[1], 100, activation_function=tf.nn.tanh)
+l1 = add_layer(xs, x_train.shape[1], 100, activation_function=tf.nn.sigmoid)
+# l1 = add_layer(xs, x_train.shape[1], 100, activation_function=tf.nn.tanh)
 # l1 = add_layer(xs, x_train.shape[1], 100, activation_function=tf.nn.relu)
-"""
-relu 激活函数存在梯度消失现象
-"""
+l1 = layer1 = tf.nn.dropout(l1, drop_out)
 
+# l2 = add_layer(l1, 100, 10, activation_function=tf.nn.sigmoid)
 l2 = add_layer(l1, 100, 10, activation_function=tf.nn.tanh)
+# l2 = add_layer(l1, 100, 10, activation_function=tf.nn.relu)
+l2 = tf.nn.dropout(l2, drop_out)
 
 prediction = add_layer(l2, 10, 1, activation_function=None)
 
 loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
                                     reduction_indices=[1]))
 
-train_step = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
+train_step = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(loss)
 
 init = tf.global_variables_initializer()
 
@@ -80,23 +81,21 @@ with tf.Session() as sess:
     sess.run(init)
 
     # Fit all training data
-    for i in range(100000):
+    for i in range(1000000):
 
-        sess.run(train_step, feed_dict={xs: x_train, ys: y_train})
+        sess.run(train_step, feed_dict=feed_dict_train)
         if i % 50 == 0:
-            # to see the step improvement
-            # print(sess.run(loss, feed_dict={xs: x_train, ys: y_train}))
             a = sess.run(loss, feed_dict={xs: x_test, ys: y_test})
             print(a, type(a))
 
             # 当误差达到阈值 储存结果
-            if float(a) < 0.1:
+
+            if float(a) < 0.02:
                 y_pre = sess.run(prediction, feed_dict={xs: test_df})
                 y_pre = y_mm.inverse_transform(y_pre)
                 pre = pd.DataFrame(y_pre, columns=["0"])
                 pre = pre["0"]
                 save_result(list(pre))
-
 
 """
 画出平面图形 
