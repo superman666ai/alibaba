0bedaf2b12b5709622b83f051b8ca9150b58f4b5
keepingoner <keepingoner@163.com>
2019-04-02 17:06:25 +0800

    del

---
+++ python_exa/父类调用子类方法.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2019-04-02 15:00
+# @Author  : jian
+# @File    : 父类调用子类方法.py
+from operator import methodcaller
+
+
+class B():
+    pass
+
+
+class A(B):
+    pass
+
+
+class C(B):
+    def pr(self):
+        print("11111")
+
+
+a = A()
+
+print(hasattr(a, "pr"))
+getattr(a, 'pr')()
+
+
+# print(B.__bases__)
+
+
+def findbases(kls, topclass):
+    retval = list()
+    for base in kls.__bases__:
+        if issubclass(base, topclass):
+            retval.extend(findbases(base, topclass))
+            retval.append(base)
+    return retval
